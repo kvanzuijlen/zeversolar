5f3a4301ad3691b481765826fc81ff6f5f3aa5ef
Derek King <king.dm49@gmail.com>
2023-02-05 21:20:45 +0800

    Only split the text once

--- src/zeversolar/__init__.py
+++ src/zeversolar/__init__.py
@@ -74,18 +74,16 @@ class ZeverSolarParser:
     def __init__(self, zeversolar_response: str):
         self.zeversolar_response = zeversolar_response
 
-    def _get_value(self, value: IntEnum) -> str:
+    def parse(self) -> ZeverSolarData:
         response_parts = self.zeversolar_response.split()
-        return response_parts[value.value]
 
-    def parse(self):
-        wifi_enabled = bool(self._get_value(Values.WIFI_ENABLED))
-        mac_address = self._get_value(Values.MAC_ADDRESS)
-        registry_key = self._get_value(Values.REGISTRY_KEY)
-        hardware_version = self._get_value(Values.HARDWARE_VERSION)
-        software_version = self._get_value(Values.SOFTWARE_VERSION)
-        communication_status = bool(self._get_value(Values.COMMUNICATION_STATUS))
-        serial_number = self._get_value(Values.SERIAL_NUMBER)
+        wifi_enabled = bool(response_parts[Values.WIFI_ENABLED])
+        mac_address = response_parts[Values.MAC_ADDRESS]
+        registry_key = response_parts[Values.REGISTRY_KEY]
+        hardware_version = response_parts[Values.HARDWARE_VERSION]
+        software_version = response_parts[Values.SOFTWARE_VERSION]
+        communication_status = bool(response_parts[Values.COMMUNICATION_STATUS])
+        serial_number = response_parts[Values.SERIAL_NUMBER]
 
         if hardware_version.upper() == "M10":
             hardware_specific_values = M10Values
@@ -94,11 +92,12 @@ class ZeverSolarParser:
         else:
             raise ZeverSolarNotSupported(f"This {hardware_version=} is not yet supported")
 
-        pac = Watt(int(self._get_value(hardware_specific_values.PAC)))
-        energy_today = kWh(self._fix_leading_zero(self._get_value(hardware_specific_values.ENERGY_TODAY)))
-        status = StatusEnum(self._get_value(hardware_specific_values.STATUS))
-        reported_time = self._get_value(Values.REPORTED_TIME)
-        reported_date = self._get_value(Values.REPORTED_DATE)
+        pac = Watt(int(response_parts[hardware_specific_values.PAC]))
+        energy_today = kWh(self._fix_leading_zero(response_parts[hardware_specific_values.ENERGY_TODAY]))
+        status = StatusEnum(response_parts[hardware_specific_values.STATUS])
+
+        reported_time = response_parts[Values.REPORTED_TIME]
+        reported_date = response_parts[Values.REPORTED_DATE]
 
         reported_datetime = datetime.strptime(f"{reported_date} {reported_time}", "%d/%m/%Y %H:%M")
 
