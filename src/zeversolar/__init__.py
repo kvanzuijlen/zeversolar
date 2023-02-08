0f913ea42992d3c8bbf6e6e1b14876222ed43a5f
Derek King <king.dm49@gmail.com>
2023-02-09 04:32:13 +0800

    Now asynchronous, and using aiohttp The ZeverSolarParser class is now:   async def zeversolar_parse(zeversolar_response: str) -> ZeverSolarData

--- src/zeversolar/__init__.py
+++ src/zeversolar/__init__.py
@@ -1,11 +1,12 @@
 import dataclasses
 import typing
+import asyncio
 import urllib.parse
 from datetime import datetime, timedelta
 from enum import Enum, IntEnum
 from dataclasses import MISSING
 
-import requests
+import aiohttp
 
 from zeversolar.exceptions import ZeverSolarTimeout, ZeverSolarHTTPError, ZeverSolarHTTPNotFound, ZeverSolarInvalidData
 
@@ -53,109 +54,105 @@ class ZeverSolarData:
     status: StatusEnum
 
 
-class ZeverSolarParser:
-    def __init__(self, zeversolar_response: str):
-        self.zeversolar_response = zeversolar_response
+async def zeversolar_parse(zeversolar_response: str) -> ZeverSolarData:
+    response_parts = zeversolar_response.split()
 
-    def parse(self) -> ZeverSolarData:
-        response_parts = self.zeversolar_response.split()
+    if len(response_parts) <= Values.NUM_INVERTERS:
+        raise ZeverSolarInvalidData()
 
-        if len(response_parts) <= Values.NUM_INVERTERS:
-            raise ZeverSolarInvalidData()
+    wifi_enabled = response_parts[Values.WIFI_ENABLED] == "1"
+    serial_or_registry_id = response_parts[Values.SERIAL_OR_REGISTRY_ID]
+    registry_key = response_parts[Values.REGISTRY_KEY]
+    hardware_version = response_parts[Values.HARDWARE_VERSION]
+    software_version = response_parts[Values.SOFTWARE_VERSION]
 
-        wifi_enabled = response_parts[Values.WIFI_ENABLED] == "1"
-        serial_or_registry_id = response_parts[Values.SERIAL_OR_REGISTRY_ID]
-        registry_key = response_parts[Values.REGISTRY_KEY]
-        hardware_version = response_parts[Values.HARDWARE_VERSION]
-        software_version = response_parts[Values.SOFTWARE_VERSION]
+    reported_time = response_parts[Values.REPORTED_TIME]
+    reported_date = response_parts[Values.REPORTED_DATE]
+    try:
+        reported_datetime = datetime.strptime(f"{reported_date} {reported_time}", "%d/%m/%Y %H:%M")
+    except ValueError:
+        raise ZeverSolarInvalidData()
 
-        reported_time = response_parts[Values.REPORTED_TIME]
-        reported_date = response_parts[Values.REPORTED_DATE]
-        try:
-            reported_datetime = datetime.strptime(f"{reported_date} {reported_time}", "%d/%m/%Y %H:%M")
-        except ValueError:
-            raise ZeverSolarInvalidData()
+    communication_status = response_parts[Values.COMMUNICATION_STATUS]
+    if communication_status.startswith(StatusEnum.OK.value):
+        communication_status = True
+    elif communication_status.startswith(StatusEnum.ERROR.value):
+        communication_status = False
+    else:
+        communication_status = communication_status == "0"
 
-        communication_status = response_parts[Values.COMMUNICATION_STATUS]
-        if communication_status.startswith(StatusEnum.OK.value):
-            communication_status = True
-        elif communication_status.startswith(StatusEnum.ERROR.value):
-            communication_status = False
-        else:
-            communication_status = communication_status == "0"
+    try:
+        num_inverters = int(response_parts[Values.NUM_INVERTERS])
+    except ValueError:
+        raise ZeverSolarInvalidData()
 
-        try:
-            num_inverters = int(response_parts[Values.NUM_INVERTERS])
-        except ValueError:
-            raise ZeverSolarInvalidData()
+    # inverters = {}  # {serial_number: pac,energy_today,status}
+    # for i in range(min(num_inverters, 5)):
+    # Just parsing one inverter for now though
+    if num_inverters < 1:
+        raise ZeverSolarInvalidData()
 
-        # inverters = {}  # {serial_number: pac,energy_today,status}
-        # for i in range(min(num_inverters, 5)):
-        # Just parsing one inverter for now though
-        if num_inverters < 1:
-            raise ZeverSolarInvalidData()
+    if len(response_parts) < Values.INVERTERS + num_inverters*4 + 1:
+        raise ZeverSolarInvalidData()
 
-        if len(response_parts) < Values.INVERTERS + num_inverters*4 + 1:
-            raise ZeverSolarInvalidData()
+    index = Values.INVERTERS
 
-        index = Values.INVERTERS
+    serial_number = response_parts[index]
+    index += 1
 
-        serial_number = response_parts[index]
+    try:
+        pac = Watt(int(response_parts[index]))
+    except ValueError:
+        # ? = response_parts[index]
         index += 1
-
         try:
             pac = Watt(int(response_parts[index]))
         except ValueError:
-            # ? = response_parts[index]
-            index += 1
-            try:
-                pac = Watt(int(response_parts[index]))
-            except ValueError:
-                raise ZeverSolarInvalidData()
-        index += 1
-
-        try:
-            energy_today = kWh(self._fix_leading_zero(response_parts[index]))
-        except ValueError:
             raise ZeverSolarInvalidData()
-        index += 1
+    index += 1
 
-        try:
-            status = StatusEnum(response_parts[index])
-        except ValueError:
-            raise ZeverSolarInvalidData()
-        index += 1
+    try:
+        energy_today = kWh(await _fix_leading_zero(response_parts[index]))
+    except ValueError:
+        raise ZeverSolarInvalidData()
+    index += 1
 
-        # We don't necessarily know how many fields in each inverter if more than one
-        # try:
-        #     meter_status = StatusEnum(response_parts[index])
-        # except ValueError:
-        #     raise ZeverSolarInvalidData()
-        # index += 1
-        # if len(response_parts) < index + 4:
-        #     raise ZeverSolarInvalidData()
+    try:
+        status = StatusEnum(response_parts[index])
+    except ValueError:
+        raise ZeverSolarInvalidData()
+    index += 1
 
-        return ZeverSolarData(
-            wifi_enabled=wifi_enabled,
-            serial_or_registry_id=serial_or_registry_id,
-            registry_key=registry_key,
-            hardware_version=hardware_version,
-            software_version=software_version,
-            communication_status=communication_status,
-            num_inverters=num_inverters,
-            serial_number=serial_number,
-            pac=pac,
-            energy_today=energy_today,
-            status=status,
-            reported_datetime=reported_datetime,
-        )
+    # We don't necessarily know how many fields in each inverter if more than one
+    # try:
+    #     meter_status = StatusEnum(response_parts[index])
+    # except ValueError:
+    #     raise ZeverSolarInvalidData()
+    # index += 1
+    # if len(response_parts) < index + 4:
+    #     raise ZeverSolarInvalidData()
 
-    @staticmethod
-    def _fix_leading_zero(string_value: str) -> float:
-        split_values = string_value.split(".")
-        if len(decimals := split_values[1]) == 1:
-            string_value = f"{split_values[0]}.0{decimals}"
-        return float(string_value)
+    return ZeverSolarData(
+        wifi_enabled=wifi_enabled,
+        serial_or_registry_id=serial_or_registry_id,
+        registry_key=registry_key,
+        hardware_version=hardware_version,
+        software_version=software_version,
+        communication_status=communication_status,
+        num_inverters=num_inverters,
+        serial_number=serial_number,
+        pac=pac,
+        energy_today=energy_today,
+        status=status,
+        reported_datetime=reported_datetime,
+    )
+
+
+async def _fix_leading_zero(string_value: str) -> float:
+    split_values = string_value.split(".")
+    if len(decimals := split_values[1]) == 1:
+        string_value = f"{split_values[0]}.0{decimals}"
+    return float(string_value)
 
 
 class ZeverSolarClient:
@@ -164,50 +161,54 @@ class ZeverSolarClient:
             # noinspection HttpUrlsUsage
             host = f"http://{host}"
         self.host = urllib.parse.urlparse(url=host).netloc.strip("/")
-        self._timeout = timedelta(seconds=5).total_seconds()
+        self._timeout = aiohttp.ClientTimeout(total=10)
         self._retries = 3
         self._serial_number = MISSING
 
-    def get_data(self) -> ZeverSolarData:
+    async def get_data(self) -> ZeverSolarData:
         exception = None
-        for _ in range(self._retries):
-            try:
-                response = requests.get(url=f"http://{self.host}/home.cgi", timeout=self._timeout)
-            except requests.exceptions.Timeout:
-                exception = ZeverSolarTimeout()
-                continue
-            if response.status_code != 200:
-                if response.status_code == 404:
-                    raise ZeverSolarHTTPNotFound()
-                raise ZeverSolarHTTPError()
-            try:
-                data = ZeverSolarParser(zeversolar_response=response.text).parse()
-            except ZeverSolarInvalidData as e:
-                exception = e
-                continue
-            return data
+        async with aiohttp.ClientSession(timeout=self._timeout) as session:
+            for i in range(self._retries):
+                try:
+                    async with session.get(f"http://{self.host}/home.cgi") as resp:
+                        if resp.status != 200:
+                            if resp.status == 404:
+                                raise ZeverSolarHTTPNotFound()
+                            raise ZeverSolarHTTPError()
+                        text = await resp.text()
+                        try:
+                            data = await zeversolar_parse(zeversolar_response=text)
+                        except ZeverSolarInvalidData as err:
+                            exception = err
+                            if i < self._retries-1:
+                                asyncio.sleep(self._timeout.total)
+                            continue
+                        return data
+                except asyncio.TimeoutError:
+                    exception = ZeverSolarTimeout()
+                    continue
         raise exception
 
-    def power_on(self):
-        return self.ctrl_power(mode=PowerMode.ON)
+    async def power_on(self):
+        return await self.ctrl_power(mode=PowerMode.ON)
 
-    def power_off(self):
-        return self.ctrl_power(mode=PowerMode.OFF)
+    async def power_off(self):
+        return await self.ctrl_power(mode=PowerMode.OFF)
 
-    def ctrl_power(self, mode: PowerMode):
+    async def ctrl_power(self, mode: PowerMode):
         if self._serial_number is MISSING:
-            self._serial_number = self.get_data().serial_number
+            data = await self.get_data()
+            self._serial_number = data.serial_number
 
-        response = requests.post(
-            url=f"http://{self.host}/inv_ctrl.cgi",
-            data={'sn': self._serial_number, 'mode': mode.value},
-            timeout=self._timeout,
-        )
-        try:
-            response.raise_for_status()
-        except requests.exceptions.Timeout:
-            raise ZeverSolarTimeout()
-        except requests.exceptions.HTTPError:
-            if response.status_code == 404:
-                raise ZeverSolarHTTPNotFound()
-            raise ZeverSolarHTTPError()
+        async with aiohttp.ClientSession(timeout=self._timeout) as session:
+            try:
+                async with session.post(
+                        url=f"http://{self.host}/inv_ctrl.cgi",
+                        data={"sn": self._serial_number, "mode": mode.value},
+                    ) as resp:
+                    if resp.status != 200:
+                        if resp.status == 404:
+                            raise ZeverSolarHTTPNotFound()
+                        raise ZeverSolarHTTPError()
+            except asyncio.TimeoutError:
+                raise ZeverSolarTimeout()
